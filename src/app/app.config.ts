d0c7495946b333e1e5edd04d14986afb3ad8bade
Fabian Meier <fabian.91m@gmail.com>
2024-10-06 23:35:12 +0200

    Add pwa to the app. No config changes done.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,20 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, isDevMode, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter, withViewTransitions} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {provideServiceWorker} from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes, withViewTransitions({
       skipInitialTransition: true,
-    })), provideAnimationsAsync(),
+    })),
+    provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ]
 };
